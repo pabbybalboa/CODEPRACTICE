d02c1f370053561e3462e0ea10e53dd4f8e7e668
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-04-06 22:11:40 -0700

    Logical Operators Tasks

--- JS FUNDAMENTALS/PRACTICE.js
+++ JS FUNDAMENTALS/PRACTICE.js
@@ -44,3 +44,80 @@ let result = (a + b < 4) ? 'Below' : 'over';
 
 //Rewrite using multiple ternary operators. Javascript.info fundamentals conditions task 4
 let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : '';
+
+
+//LOGICAL OPERATORS
+// What is the code below going to output?
+alert ( null || 2 || undefined )
+// 2 because it's the first truthy value
+
+//What will the code below output?
+alert ( alert(1) || 2 || alert(3) )
+//1 then 2. Alert runs showing 1 but returns alert undefined. 2 is truthy, so the execution is halted and shows on outer alert.
+
+//What is the code below going to show?
+alert ( 1 && null && 2 )
+// Null because it's the first falsy value from the list.
+
+//What is the code below going to show?
+alert ( alert(1) && alert(2) )
+//1 then undefined. The first alert runs and shows 1 (no meaningful return) then && runs and returns undefined as falsey value.
+
+//What will the result be?
+alert( null || 2 && 3 || 4 )
+//3. Treats the 2 && 3 as parentheses and the && takes precedence so returns 3 as the first truthy value. Same as null ( 2 && 3 = 3) 4.
+
+//Write an if condition to check that age is between 14 and 90 inclusively. 
+let age 
+    if (age >= 14 && age <= 90){
+}
+
+//Write an if condition to check that age is not between 14 and 90 inclusively. Create two, one using not, the second without.
+let age1
+    if ( !(age1 >= 14 && age1 <= 90)){
+    }
+    if (age1 < 14 && age1 > 90){
+    }
+
+//Which of these alerts are going to execute?
+if (-1 || 0) alert( 'first' );
+if (-1 && 0) alert( 'second' );
+if (null || -1 && 1) alert( 'third' );
+//First and third. First- result is -1, truthy so it runs. Second- doesn't run, falsey. Third- Executes and results 1.
+
+//Write code which asks for login with promt.
+//If visitor enters "Admin", promt for pasword, if input is an empty line or esc-show canceled, if another string, show "I don't know you."
+//The password is checked: if equals "TheMaster" then show "Welcome!". another string, show "wrong password". empty string or cancelled, show "canceled".
+//My try
+let prompt = "Who's there?"
+let input
+let password
+    if (input = "Admin"){
+        if (password = "TheMaster"){
+            alert("Welcome!")
+        }else if (password = !"TheMaster"){
+            alert("Wrong Password")
+        }else 
+            alert("Canceled")
+        
+    }else if( input = !"Admin"){
+        alert("I don't know you.")
+    }else
+        alert('Canceled')
+    
+//Solution
+let userName = promt("Who's there?", '');
+if (userName === 'Admin'){
+    let pass = promt('Password?', '');
+    if (pass === 'TheMaster'){
+        alert('Welcome')
+    }else if (pass === '' || pass === null){
+        alert('Canceled')
+    }else{
+        alert('Wrong Password')
+    }
+}else if (userName === '' || userName === null){
+    alert('Canceled')
+}else{
+    alert("I don't know you.")
+}
