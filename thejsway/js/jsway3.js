41fb89a261a6800b12e2403d8ba9ba2996e7d4cd
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-05-01 20:17:08 -0700

    JSWay. Objects.

--- thejsway/js/jsway3.js
+++ thejsway/js/jsway3.js
@@ -42,3 +42,81 @@ while(true){
     command.push(input)
     console.log(command)
 }
+
+
+//OBJECTS
+
+//Create an RPG character object with a name, health points and strength points. Then add experience points and a method to describe azuroth.
+
+const character = {
+    name: "Azuroth",
+    health: 150,
+    strength: 15000,
+    xp: 0,
+
+    describe() {
+        return `${this.name} has ${this.health} health points, ${this.strength} as strength, and ${this.xp} XP points.`
+    }
+}
+character.health -= 20 //Takes an arrow to the knee.
+character.strength += 1500 //Drinks a strength potion.
+character.xp += 2000 //Azuroth defeats his foe.
+console.log(character.describe())
+
+
+//Complete the following program to add the dog object definition:
+
+const dog = {
+    name: "Lotus",
+    species: "Schanuzer",
+    size: "30 inches",
+
+    bark(){
+        return "Woof woof!"
+    }
+}
+
+console.log(`${dog.name} is a ${dog.species} dog measuring ${dog.size}`)
+console.log(`Look, a cat! ${dog.name} barks: ${dog.bark()}`)
+
+
+//Complete the following program to add the circle object definition. The radius value is the input by the user.
+
+const r = Number(prompt("Enter the circle radius:"))
+const circle = {
+    radius: r,
+
+    circumfrence(){
+        return (2 * (Math.PI) * r)
+    },
+
+    area(){
+        return ((Math.PI) * (r ** 2))
+    }
+}
+
+console.log(`It's circumfrence is ${circle.circumfrence()}`)
+console.log(`Its area is ${circle.area()}`)
+
+
+//Write a program that creates an account object with the following characteristics: name, balance, credit, and describe.
+
+const account = {
+    name: "Alex",
+    balance: 0,
+
+    credit(){
+        let number = 0
+        return number += balance
+    },
+
+    describe(){
+        return `owner: ${this.name}, balance ${this.balance}`
+    }
+}
+console.log(account.describe())
+account.balance += 250 //credit 250
+console.log(account.describe())
+account.balance -= 80 //debit 80
+console.log(account.describe())
+
