d728abff82534f04c72e0f04b1353eb21f35b03f
PabbyBalboa <pablo.dav.ruiz@gmail.com>
2022-05-24 15:04:00 -0700

    update

--- arraycardio/js/arraycardio.js
+++ arraycardio/js/arraycardio.js
@@ -82,4 +82,4 @@
 
       //ANSWERS: https://github.com/wesbos/JavaScript30/blob/master/04%20-%20Array%20Cardio%20Day%201/index-FINISHED.html
 
-      //do again
+      //
--- thejsway/js/jsway3.js
+++ thejsway/js/jsway3.js
@@ -147,3 +147,17 @@ const glacius = {
         return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points.`
     }
 }
+
+//creating a character class and constructor
+
+class Character {
+    constructor(name, health, strength){
+        this.name = name
+        this.health = health
+        this.strength = strength
+        this.xp = 0
+    }
+    describe(){
+        return `${this.name} has ${this.health} health points, ${this.strength} as strength and ${this.xp} XP points.`
+    }
+}
